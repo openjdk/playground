5573c61e9d4653bcabe3fedb7c6c372cdf4b4297
Zhao Song <zhao.song@oracle.com>
2025-09-17 09:09:21 -0700

    security

--- src/java.base/share/classes/sun/security/action/GetBooleanAction.java
+++ src/java.base/share/classes/sun/security/action/GetBooleanAction.java
@@ -21,6 +21,7 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
+ * security
  */
 
 package sun.security.action;
