72a8773d8669e60daab974052421bf7450fbe24b
Zhao Song <zsong@openjdk.org>
2025-09-17 23:16:13 +0000

    Test SKARA-2065

--- src/java.base/aix/classes/sun/nio/ch/AixAsynchronousChannelProvider.java
+++ src/java.base/aix/classes/sun/nio/ch/AixAsynchronousChannelProvider.java
@@ -22,6 +22,7 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
+ * update
  */
 
 package sun.nio.ch;
